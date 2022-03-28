31b5037ec42567b32a7452b34ea1cdf52ff0fa77
Matteo Anselmo <matteo.anselmo96@gmail.com>
2022-03-28 16:35:03 +0200

    implementazione header main footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,154 @@
 <template>
   <div>
-    <h1>Footer</h1>
+    <section id="first-footer-section">
+      <div class="container centralized-item">
+        <div class="lists-wrapper centralized-item">
+          <div>
+          <h2 class="my-text-white">DC COMICS</h2>
+          <nav>
+            <ul class="footer-list">
+              <li>
+                <a href="#">Characters</a> 
+              </li>
+              <li>
+                <a href="#">Comics</a>
+              </li>
+              <li>
+                <a href="#">Movies</a>
+              </li>
+              <li>
+                <a href="#">TV</a>
+              </li>
+              <li>
+                <a href="#">Games</a>
+              </li>
+              <li>
+                <a href="#">Videos</a>
+              </li>
+              <li>
+                <a href="#">News</a>
+              </li>
+            </ul>
+          </nav>
+          <h2 class="my-text-white">SHOP</h2>
+          <nav>
+            <ul class="footer-list">
+              <li>
+                <a href="#">Shop DC </a> 
+              </li>
+              <li>
+                <a href="#">Shop DC Collectibles</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div>
+          <h2 class="my-text-white">DC</h2>
+          <nav>
+            <ul class="footer-list">
+              <li>
+                <a href="#">Terms of use</a> 
+              </li>
+              <li>
+                <a href="#">Privacy Policy (News)</a>
+              </li>
+              <li>
+                <a href="#">Ad Choising</a>
+              </li>
+              <li>
+                <a href="#">Advertising</a>
+              </li>
+              <li>
+                <a href="#">Jobs</a>
+              </li>
+              <li>
+                <a href="#">Subscription</a>
+              </li>
+              <li>
+                <a href="#">Talent Workshop</a>
+              </li>
+              <li>
+                <a href="#">CPSC Certificates</a>
+              </li>
+              <li>
+                <a href="#">rating</a>
+              </li>
+              <li>
+                <a href="#">Shop Help</a>
+              </li>
+              <li>
+                <a href="#">Contact Us</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div>
+          <h2 class="my-text-white">SITES</h2>
+          <nav>
+            <ul class="footer-list">
+              <li>
+                <a href="#">DC</a> 
+              </li>
+              <li>
+                <a href="#">MAD Magazine</a>
+              </li>
+              <li>
+                <a href="#">DC Kinds</a>
+              </li>
+              <li>
+                <a href="#">DC Universe</a>
+              </li>
+              <li>
+                <a href="#">DC Power Visa</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        </div>
+        <div id="logo-bg">
+        </div>
+      </div>
+      
+    </section>
+    <section id="last-footer-section">
+      <div class="container centralized-item">
+        <div>
+          <button>SIGN-UP NOW!</button>
+        </div>
+        <div class="social-wrapper centralized-item">
+          <h2>FOLLOW US</h2>
+          <nav>
+            <ul class="footer-list centralized-item">
+              <li>
+                <a href="#">
+                  <img src="../assets/img/footer-facebook.png" alt="facebook logo">
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="../assets/img/footer-twitter.png" alt="twitterlogo">
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="../assets/img/footer-youtube.png" alt="youtube logo">
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="../assets/img/footer-pinterest.png" alt="pintarest logo">
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img src="../assets/img/footer-periscope.png" alt="periscope logo">
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -12,5 +160,44 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
+@import "../assets/scss/partials/_variables.scss";
+#first-footer-section{
+  background-image: url("../assets/img/footer-bg.jpg");
+  .lists-wrapper{
+    justify-content: start;
+    align-items: flex-start;
+    div{
+      padding-right: 1rem;
+    }
+  }
+  #logo-bg{
+    background-image: url("../assets/img/dc-logo-bg.png");
+    height: 23rem;
+    width: 70%;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+}
+#last-footer-section{
+  background-color: #303030;
+  padding: 2rem 0;
+  button{
+    font-size: 1.2rem;
+    background-color: #303030;
+    color: white;
+    text-decoration: none;
+    border: 2px solid $brandColor;
+    padding: 1.2rem;
+  }
+  .social-wrapper{
+    h2{
+      color: $brandColor;
+    }
+  }
+  .footer-list{
+    li{
+      margin: 0 .4rem;
+    }
+  }
+}
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,7 +6,28 @@
       </div>
     </section>
     <section id="card-section">
-
+      <div class="container centralized-item">
+        <div class="centralized-item card-wrapper">
+          <img src="../assets/img/buy-comics-digital-comics.png" alt="digital comics card">
+          <p>digital comics</p>
+        </div>
+        <div class="centralized-item card-wrapper">
+          <img src="../assets/img/buy-comics-merchandise.png" alt="digital merchandise">
+          <p>dc merchandise</p>
+        </div>
+        <div class="centralized-item card-wrapper">
+          <img src="../assets/img/buy-comics-subscriptions.png" alt="digital comics subscription">
+          <p>subscription</p>
+        </div>
+        <div class="centralized-item card-wrapper">
+          <img src="../assets/img/buy-comics-shop-locator.png" alt="digital comics shop locator">
+          <p>comic shop locator</p>
+        </div>
+        <div class="centralized-item card-wrapper">
+          <img class="my-svg-card" src="../assets/img/buy-dc-power-visa.svg" alt="digital comics card">
+          <p>dc power visa</p>
+        </div>
+      </div>
     </section>
   </div>
 </template>
@@ -19,10 +40,28 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+@import "../assets/scss/partials/_variables.scss";
 #main-content{
   background-color: black;
   color: white;
   padding: 4rem 0;
+}
+#card-section{
+  background-color: $brandColor;
+  padding: 2rem 0;
+  img{
+    width: 25%;
+  }
+  p{
+    color: white;
+    text-transform: uppercase;
+  }
+  .card-wrapper{
+    justify-content: space-evenly;
+    .my-svg-card{
+      width: 7rem;
+    }
+  }
 }
 
 </style>
